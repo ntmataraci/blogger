cfd37eeff8814bb28549b9e12f48b921ca13967c
Nevzat Mataraci <91319943+ntmataraci@users.noreply.github.com>
2022-03-24 00:44:29 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration (use your keys)
 const firebaseConfig = {
-  apiKey: "AIzaSyCSfteYHqdtrxjfzUt-W7Ued8jo4A4tfUs",
-  authDomain: "users-11a8f.firebaseapp.com",
-  projectId: "users-11a8f",
-  storageBucket: "users-11a8f.appspot.com",
-  messagingSenderId: "744028285891",
-  appId: "1:744028285891:web:f554a987712ae79be3ab76"
+  apiKey: ,
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
 };
 
 // Initialize Firebase
